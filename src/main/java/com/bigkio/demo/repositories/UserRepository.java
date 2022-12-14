5a1709c223d7284be0627503fd000b5c7632c4d5
Iury V <iurygol@gmail.com>
2022-12-13 22:48:38 -0300

    Create User and Task repositories

---
+++ src/main/java/com/bigkio/demo/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.bigkio.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bigkio.demo.models.User;
+
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    
+
+}
