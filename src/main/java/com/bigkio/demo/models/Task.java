864fc904c06a12e725adc4980955c801cb547585
Iury V <iurygol@gmail.com>
2022-12-14 22:54:00 -0300

    Create UserService

--- src/main/java/com/bigkio/demo/models/Task.java
+++ src/main/java/com/bigkio/demo/models/Task.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+
 @Entity
 @Table(name = Task.TABLE_NAME)
 public class Task {
